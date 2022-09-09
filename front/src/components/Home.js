a1a7a4e550be9ad9766ddfc3dd1efeaedc152045
Matthew Vargas <49174708+Automaton1989@users.noreply.github.com>
2022-09-09 10:13:11 -0700

    Sep 9 changes to home page mobile and tablet view

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,7 +1,7 @@
 import "../App.css";
 import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import pic from "./img/main-background.jpeg"
+import pic from "./img/Halloween.jpg"
 
 function Home() {
 	const [products, setProducts] = useState([]);
@@ -20,12 +20,34 @@ function Home() {
 		fetchData();
 	}, [])
 
-
 /*
 
-<Link to = "/login">
-	Go LOGIN
-</Link>
+<div id = "home-page-creators" className = "row">
+	<div className = "col-12">
+		<div className="card">
+		  <img className="card-img-top" src="#" />
+		  <div className="card-body">
+		    <p className="card-text">TBD</p>
+		  </div>
+		</div>
+	</div>
+	<div className = "col-12">
+		<div className="card">
+		  <img className="card-img-top" src="#" />
+		  <div className="card-body">
+		    <p className="card-text">TBD</p>
+		  </div>
+		</div>
+	</div>
+	<div className = "col-12">
+		<div className="card">
+		  <img className="card-img-top" src="#" />
+		  <div className="card-body">
+		    <p className="card-text">TBD</p>
+		  </div>
+		</div>
+	</div>
+</div>
 
 */
 
@@ -33,43 +55,26 @@ function Home() {
 		<div className = "container-fluid">
 			<div id = "home-page-main" className = "row">
 				<div className = "col-12">
-					<img src = {pic} id = "home-page-main-img" />
-				</div>
-			</div>
-			<div id = "home-page-creators" className = "row">
-				<div className = "col-12">
-					<div className="card">
-					  <img className="card-img-top" src="#" />
-					  <div className="card-body">
-					    <p className="card-text">CARD 1</p>
-					  </div>
+					<img src = {pic} id = "home-page-main-img" alt="Auto and Kailani Halloween background" />
+					<div class = "container-img-sm d-block d-sm-none">
+						<div class="centered">Centered</div>
 					</div>
-				</div>
-				<div className = "col-12">
-					<div className="card">
-					  <img className="card-img-top" src="#" />
-					  <div className="card-body">
-					    <p className="card-text">CARD 2</p>
-					  </div>
-					</div>
-				</div>
-				<div className = "col-12">
-					<div className="card">
-					  <img className="card-img-top" src="#" />
-					  <div className="card-body">
-					    <p className="card-text">CARD 3</p>
-					  </div>
+					<div class = "container-img-md d-none d-md-block d-lg-none">
+						<div class="centered">Centered</div>
+					</div>					
+					<div class = "container-img-lg d-none d-sm-none d-md-none d-lg-block">
+						<div class="centered">Centered</div>
 					</div>
 				</div>
 			</div>
-			<div id = "home-page-cardbreaks" className = "row">
+			<div id = "home-page-streams" className = "row">
 				<div className = "col-12">
 					<div className="card">
-					  <h5 className="card-header">Card Breaks</h5>
+					  <h5 className="card-header">VTuber, Content Creator</h5>
 					  <div className="card-body">
-					    <h5 className="card-title">Auto's Card Breaks</h5>
-					    <p className="card-text">Wanna join my card breaks for new Pokemon and Wixoss sets?</p>
-					    <Link to="#" class="btn btn-primary">Sign up for a break!</Link>
+					    <h5 className="card-title">Auto & Kailani</h5>
+					    <p className="card-text">I stream games on Twitch, upload videos to YouTube and Tiktok, and we bring positive and comfy vibes every day</p>
+					    <Link to="/about" class="btn btn-primary">About Me</Link>
 					  </div>
 					</div>
 				</div>
@@ -80,8 +85,32 @@ function Home() {
 					  <h5 className="card-header">Charity Events</h5>
 					  <div className="card-body">
 					    <h5 className="card-title">Yearly Charity Stream Marathon</h5>
-					    <p className="card-text">Next charity stream is March 16-20, 2022 in support of Monterey Bay Aquarium!</p>
+					    <p className="card-text">Next charity stream is in the works! We support Monterey Bay Aquarium!</p>
 					    <Link to="#" class="btn btn-primary">View Events</Link>
+					  </div>
+					</div>
+				</div>
+			</div>
+			<div id = "" className = "row">
+				<div className = "col-12">
+					<div className="card">
+					  <h5 className="card-header">Merch</h5>
+					  <div className="card-body">
+					    <h5 className="card-title">I have cool merch!</h5>
+					    <p className="card-text">I have a streamelements merch store open!  Filled with great ways to show your support, and holiday themed material!</p>
+					    <a href="https://merch.streamelements.com/automaton1989" target="_blank" rel="noreferrer" class="btn btn-primary">Go to Store</a>
+					  </div>
+					</div>
+				</div>
+			</div>
+			<div id = "" className = "row">
+				<div className = "col-12">
+					<div className="card">
+					  <h5 className="card-header">Socials</h5>
+					  <div className="card-body">
+					    <h5 className="card-title">Find my content!</h5>
+					    <p className="card-text">You can view all my socials on carrd if you'd like to follow my content!</p>
+					    <a href="https://auto-kailani.carrd.co" target="_blank" rel="noreferrer" class="btn btn-primary">Socials</a>
 					  </div>
 					</div>
 				</div>
