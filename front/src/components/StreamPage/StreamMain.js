d0a45cac1f391c42764d51754f8659ad979398e9
Matthew Vargas <49174708+Automaton1989@users.noreply.github.com>
2023-03-14 23:01:47 -0700

    Updated the stream page with more information and styling

--- front/src/components/StreamPage/StreamMain.js
+++ front/src/components/StreamPage/StreamMain.js
@@ -1,20 +1,58 @@
 import "../.././App.css";
+import { Link } from "react-router-dom";
 
 function StreamMain() {
+	/* autokailani.herokuapp.com */
 	return(
 		<div className = "container-fluid">
-			<div className = "row">
-				<h2>Stream Page Here</h2>
+				<div className = "row">
+					<div className = "col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+						<div className="card card-stream">
+							<div id="twitch_stream_header_card" className="card-title">
+								My Stream
+							</div>
+						</div>
+					</div>
+				</div>
+			<div id = "stream_video_row" className = "row">
+				<div className = "d-block col-sm-12 col-md-8 col-lg-8 col-xl-9">
+					<iframe id="twitch_stream_video" src="https://player.twitch.tv/?channel=autowhale&parent=autokailani.herokuapp.com" title="Auto Stream Video" frameBorder="0" allowFullScreen={true} scrolling="no"></iframe>
+				</div>
+				<div className = "d-block col-sm-12 col-md-4 col-lg-4 col-xl-3">
+					<iframe
+					  title = "Auto Stream Chat"
+					  id="twitch_stream_chat"
+					  src="https://www.twitch.tv/embed/autowhale/chat?parent=localhost"
+					  >
+					</iframe>
+				</div>
 			</div>
-			<div className = "row">
-				<iframe src="https://player.twitch.tv/?channel=autowhale&parent=autokailani.herokuapp.com" title="Auto Stream Video" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
-				<iframe
-				  title = "Auto Stream Chat"
-				  id="chat_embed"
-				  src="https://www.twitch.tv/embed/autowhale/chat?parent=autokailani.herokuapp.com"
-				  height="500"
-				  width="350">
-				</iframe>
+			<div id ="stream_panels_row" className = "row">
+				<div className = "d-block col-sm-12 col-md-6 col-lg-6 col-xl-6">
+					<div className="card">
+					  <h5 className="card-header">About Me</h5>
+					  <div className="card-body">
+					    <p className="card-text">
+					    	Welcome to the stream! I am Auto (the whale), 
+					    	I recently graduated from Northeastern University MS for Computer Science program, 
+					    	and work full time as a Product Owner.
+					    </p>
+					  </div>
+					</div>
+				</div>
+				<div className = "d-block col-sm-12 col-md-6 col-lg-6 col-xl-6">
+					<div className="card">
+					  <h5 className="card-header">Rules</h5>
+					  <div className="card-body">
+					    <span className="card-text">
+					    	<ul className = "rules">Don't be a jerk either to me or anyone else in chat</ul>
+					    	<ul className = "rules">Please be respectful of others</ul>
+					    	<ul className = "rules">Any racism, Sexism, or poor manners will result in a ban.</ul>
+					    	<ul className = "rules">"Bitch" or variations of it is banned in this channel.</ul>
+					    </span>
+					  </div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
