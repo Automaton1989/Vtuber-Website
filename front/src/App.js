19ff9af0fe9e27c432c09f1db098ae00ad7f22b2
Matthew Vargas <49174708+Automaton1989@users.noreply.github.com>
2023-08-01 23:14:39 -0700

    removed stream page

--- front/src/App.js
+++ front/src/App.js
@@ -6,7 +6,6 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import CharityPage from "./components/Charity/CharityPage";
-import StreamPage from "./components/StreamPage/StreamMain.js";
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
           <Routes>
             <Route exact path = "/" element = {<Home />} />
             <Route path = "/about" element = {<AboutMe />} />
-            <Route path = "/stream" element = {<StreamPage />} />
             <Route path = "/charity" element = {<CharityPage />} />
           </Routes>
         </div>
