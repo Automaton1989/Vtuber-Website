cafd5b8e033dd743d12829ba87dd156cec72f75f
Matthew Vargas <49174708+Automaton1989@users.noreply.github.com>
2023-08-01 23:37:07 -0700

    Added Nova Stella and Stella Bear pages

--- front/src/App.js
+++ front/src/App.js
@@ -6,6 +6,8 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import CharityPage from "./components/Charity/CharityPage";
+import NovaStellaHome from './components/NovaStella/NovaStellaHome';
+import StellaBear from './components/Stella/StellaBear';
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
             <Route exact path = "/" element = {<Home />} />
             <Route path = "/about" element = {<AboutMe />} />
             <Route path = "/charity" element = {<CharityPage />} />
+            <Route path = "/Novastella" element = {<NovaStellaHome />} />
+            <Route path = "Stella" element = {<StellaBear />} />
           </Routes>
         </div>
         <Footer />
